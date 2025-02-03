3dd6b5f38bad0f90d185325bc22e9fcedd48d6ee
gatomon20xx <95043862+gatomon20xx@users.noreply.github.com>
2025-02-02 17:55:33 -0700

    Booru Set-up

---
+++ FileShare/cache/initscript/default.1738537883.d1ec938935ced7f7499bc6acd5a61471.js
@@ -0,0 +1,25 @@
+function shm_cookie_set(name, value) {
+	Cookies.set(name, value, {expires: 365, samesite: "lax", path: "/"});
+}
+function shm_cookie_get(name) {
+	return Cookies.get(name);
+}
+function shm_make_link(page, query) {
+    let base = (document.body.getAttribute("data-base-link") ?? "");
+    let joiner = base.indexOf("?") === -1 ? "?" : "&";
+    let url = base + page;
+    if(query) url += joiner + new URLSearchParams(query).toString();
+    return url;
+}
+
+function shm_log(section, ...message) {
+    window.dispatchEvent(new CustomEvent("shm_log", {detail: {section, message}}));
+}
+window.addEventListener("shm_log", function (e) {
+    console.log(e.detail.section, ...e.detail.message);
+});
+window.addEventListener("error", function (e) {
+    shm_log("Window error:", e.error);
+});
+
+//# sourceMappingURL=default.1738537883.d1ec938935ced7f7499bc6acd5a61471.js.map
---
+++ FileShare/config/shimmie.conf.php
@@ -0,0 +1,3 @@
+<?php
+define('DATABASE_DSN', 'sqlite:data/shimmie.8040f13289.sqlite');
+define('SECRET', 'c583d50777dabaacdc85ee1cf34c8da8');
